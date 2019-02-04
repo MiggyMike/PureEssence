76c609ebbc421a8fb3c1a66f1ab0ae1b152c0ac6
Michael Jennings <mjennings6@gmail.com>
2019-02-04 14:48:16 -0500

    4.3 created order controller and views

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,4 +1,10 @@
+#class SimplePagesController < ApplicationController
+#  def index
+#  end
+#end
+
 class SimplePagesController < ApplicationController
-  def index
+  def landing_page
+    @featured_product = Product.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'simple_pages/landing_page'
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
+
 end
