0a73ac69f7bd91555634c330134ee6ac8274418f
Michael Jennings <mjennings6@gmail.com>
2019-02-05 17:38:21 -0500

    4.4 Models & Associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_25_205424) do
+ActiveRecord::Schema.define(version: 2019_02_04_235437) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float "total"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
