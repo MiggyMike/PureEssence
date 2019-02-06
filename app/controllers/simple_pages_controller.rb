f5de42dedf84c9eff71dc51a7cc9c775edf0d277
Michael Jennings <mjennings6@gmail.com>
2019-02-06 17:14:10 -0500

    4.5 added carousel and rendering products

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,10 +1,10 @@
 class SimplePagesController < ApplicationController
  def index
  end
-end
 
-class SimplePagesController < ApplicationController
   def landing_page
-    @featured_product = Product.first
+    #@featured_product = Product.first
+    @products = Product.limit(3)
   end
+  
 end
