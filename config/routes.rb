7fccb57acd37ae2fe7f5bb14b3cbd7afdf50fb6f
Michael Jennings <mjennings6@gmail.com>
2019-02-11 13:28:53 -0500

    5.1 Routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#landing_page'
+  #get '/products/:id', to: 'products#show'
 
 end
