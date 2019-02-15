e02f56ae1ba8ddeff57db75dae8b4e58d6f00ae0
Michael Jennings <mjennings6@gmail.com>
2019-02-15 15:36:03 -0500

    5.2 updated error for search box to function

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(from: email,
-        to: 'your-email@example.com',
+        to: 'mjennings6@gmail.com',
         subject: "A new contact form message from #{name}")
  end
 
