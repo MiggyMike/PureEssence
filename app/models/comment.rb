976677ac34dccebae63f004eedbc431cb333ec1c
Michael Jennings <mjennings6@gmail.com>
2019-02-25 14:21:38 -0500

    5.5 authorization -2, merging comment branch to master

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :product
+
+  scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_asc, -> { order(rating: :asc) }
+
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   def self.search(search_term)
    	if Rails.env.development?
@@ -8,5 +9,13 @@ class Product < ApplicationRecord
  	  	Product.where("name ilike ?", "%#{search_term}%")
  		end
  	end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_desc.first
+  end
 
 end
