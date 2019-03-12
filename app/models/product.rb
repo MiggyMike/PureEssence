f59a616849b57e8166cdd17071a5498bcb6e913a
Michael Jennings <mjennings6@gmail.com>
2019-03-12 13:57:15 -0400

    6.2 unit test (models/product_spec.rb)

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,10 @@ class Product < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true
-  validates :body, presence: true
-  validates :user, presence: true
-  validates :product, presence: true
-  validates :rating, numericality: { only_integer: true }
+  # validates :body, presence: true
+  # validates :user, presence: true
+  # validates :product, presence: true
+  # validates :rating, numericality: { only_integer: true }
 
   def self.search(search_term)
    	if Rails.env.development?
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Product do
+  let(:product) { Product.create!(name: "race bike") }
+  let(:user) { User.create!(email: "racebike@testmail.com", password:"racebike") }
+
+  before do
+    product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+    product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+    product.comments.create!(rating: 5, user: user, body: "Great bike!")
+  end
+
+  it "returns the average rating of all comments" do
+    expect(product.average_rating).to eq 3
+  end
+
+  it "is not valid without a name" do
+    expect(Product.new(description: "Nice bike")).not_to be_valid
+  end
+
 end
